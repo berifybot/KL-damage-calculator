bb02210e72ef8a08d6c5b1f3d85df940b4fc5dd1
QuinnBerry <qberry97@outlook.com>
2024-01-22 19:33:33 -0700

    Add constructor that takes in dictionary

--- src/enemy.py
+++ src/enemy.py
@@ -5,10 +5,19 @@ class Enemy():
     base_damage = 0
     damage_type = ""
     weaknesses = []
-    def load(self, enemy_dict):
-        self.name = enemy_dict['name']
-        self.max_health = enemy_dict['max_health']
-        self.current_health = self.max_health
-        self.base_damage = enemy_dict['base_damage']
-        self.damage_type = enemy_dict['damage_type']
-        self.weaknesses = enemy_dict['weaknesses']
+
+    def __init__(self, enemy_dict = {}):
+        self.load(enemy_dict)
+
+    def load(self, enemy_dict: dict):
+        if ('name' in enemy_dict):
+            self.name = enemy_dict['name']
+        if ('max_health' in enemy_dict):
+            self.max_health = enemy_dict['max_health']
+            self.current_health = self.max_health
+        if ('base_damage' in enemy_dict):
+            self.base_damage = enemy_dict['base_damage']
+        if ('damage_type' in enemy_dict):
+            self.damage_type = enemy_dict['damage_type']
+        if ('weaknesses' in enemy_dict):
+            self.weaknesses = enemy_dict['weaknesses']
--- src/enemy_test.py
+++ src/enemy_test.py
@@ -14,15 +14,25 @@ class TestEnemy(unittest.TestCase):
         enemy = Enemy()
         self.assertIsNotNone(enemy)
 
+    def test_dict_argument_creation(self):
+        enemy = Enemy(dragon_dict)
+        self.assertIsNotNone(enemy)
+        self.assertEqual(enemy.name, dragon_dict['name'])
+        self.assertEqual(enemy.max_health, dragon_dict['max_health'])
+        self.assertEqual(enemy.current_health, dragon_dict['max_health'])
+        self.assertEqual(enemy.base_damage, dragon_dict['base_damage'])
+        self.assertEqual(enemy.damage_type, dragon_dict['damage_type'])
+        self.assertEqual(enemy.weaknesses, dragon_dict['weaknesses'])
+
     def test_load_creation(self):
         enemy = Enemy()
         enemy.load(dragon_dict)
-        self.assertEquals(enemy.name, dragon_dict['name'])
-        self.assertEquals(enemy.max_health, dragon_dict['max_health'])
-        self.assertEquals(enemy.current_health, dragon_dict['max_health'])
-        self.assertEquals(enemy.base_damage, dragon_dict['base_damage'])
-        self.assertEquals(enemy.damage_type, dragon_dict['damage_type'])
-        self.assertEquals(enemy.weaknesses, dragon_dict['weaknesses'])
+        self.assertEqual(enemy.name, dragon_dict['name'])
+        self.assertEqual(enemy.max_health, dragon_dict['max_health'])
+        self.assertEqual(enemy.current_health, dragon_dict['max_health'])
+        self.assertEqual(enemy.base_damage, dragon_dict['base_damage'])
+        self.assertEqual(enemy.damage_type, dragon_dict['damage_type'])
+        self.assertEqual(enemy.weaknesses, dragon_dict['weaknesses'])
 
 
 if __name__ == '__main__':
