862cb59527b7efe883491d83ecef7aff5351bdc6
QuinnBerry <qberry97@outlook.com>
2024-02-04 21:17:54 -0700

    Use getters for instance values in tests

--- src/damage_source_test.py
+++ src/damage_source_test.py
@@ -17,18 +17,18 @@ class TestDamageSource(unittest.TestCase):
                         test_weapon['damage_type'])
         
         self.assertIsNotNone(weapon)
-        self.assertEqual(weapon.name, test_weapon['name'])
-        self.assertEqual(weapon.base_damage, test_weapon['base_damage'])
-        self.assertEqual(weapon.element, test_weapon['element'])
-        self.assertEqual(weapon.attack_type, test_weapon['damage_type'])
+        self.assertEqual(weapon.get_name(), test_weapon['name'])
+        self.assertEqual(weapon.get_base_damage(), test_weapon['base_damage'])
+        self.assertEqual(weapon.get_element(), test_weapon['element'])
+        self.assertEqual(weapon.get_attack_type(), test_weapon['damage_type'])
 
     def test_weapon_from_dict_creation(self):
         weapon = Weapon.from_dict(test_weapon)
         
-        self.assertEqual(weapon.name, test_weapon['name'])
-        self.assertEqual(weapon.base_damage, test_weapon['base_damage'])
-        self.assertEqual(weapon.element, getattr(Element, test_weapon['element']))
-        self.assertEqual(weapon.attack_type, getattr(AttackType, test_weapon['damage_type']))
+        self.assertEqual(weapon.get_name(), test_weapon['name'])
+        self.assertEqual(weapon.get_base_damage(), test_weapon['base_damage'])
+        self.assertEqual(weapon.get_element(), getattr(Element, test_weapon['element']))
+        self.assertEqual(weapon.get_attack_type(), getattr(AttackType, test_weapon['damage_type']))
 
 if __name__ == '__main__':
     unittest.main()
--- src/entity_test.py
+++ src/entity_test.py
@@ -28,17 +28,17 @@ class TestEnemy(unittest.TestCase):
                       dragon_dict['damage_source'],
                       dragon_dict['weaknesses'])
         self.assertIsNotNone(enemy)
-        self.assertEqual(enemy.name, dragon_dict['name'])
-        self.assertEqual(enemy.max_health, dragon_dict['max_health'])
-        self.assertEqual(enemy.current_health, dragon_dict['max_health'])
-        self.assertEqual(enemy.weaknesses, dragon_dict['weaknesses'])
+        self.assertEqual(enemy.get_name(), dragon_dict['name'])
+        self.assertEqual(enemy.get_max_health(), dragon_dict['max_health'])
+        self.assertEqual(enemy.get_current_health(), dragon_dict['max_health'])
+        self.assertEqual(enemy.get_weaknesses(), dragon_dict['weaknesses'])
 
     def test_enemy_from_dict_creation(self):
         enemy = Enemy.from_dict(dragon_dict)
-        self.assertEqual(enemy.name, dragon_dict['name'])
-        self.assertEqual(enemy.max_health, dragon_dict['max_health'])
-        self.assertEqual(enemy.current_health, dragon_dict['max_health'])
-        self.assertEqual(enemy.weaknesses, dragon_dict['weaknesses'])
+        self.assertEqual(enemy.get_name(), dragon_dict['name'])
+        self.assertEqual(enemy.get_max_health(), dragon_dict['max_health'])
+        self.assertEqual(enemy.get_current_health(), dragon_dict['max_health'])
+        self.assertEqual(enemy.get_weaknesses(), dragon_dict['weaknesses'])
 
     def test_player_default_creation(self):
         player_name = "test_player"
@@ -46,21 +46,21 @@ class TestEnemy(unittest.TestCase):
         attack_speed = 5
 
         player = Player(player_name, max_health, test_weapon, attack_speed)
-        self.assertEqual(player.name, player_name)
-        self.assertEqual(player.max_health, max_health)
-        self.assertEqual(player.attack_speed, attack_speed)
+        self.assertEqual(player.get_name(), player_name)
+        self.assertEqual(player.get_max_health(), max_health)
+        self.assertEqual(player.get_attack_speed(), attack_speed)
 
     def test_player_no_optional_creation(self):
         player_name = "test_player"
         max_health = 500
         attack_speed = 5
-        weaknesses = []
+        weaknesses = ["Water", "Fire"]
 
         player = Player(player_name, max_health, test_weapon, attack_speed, weaknesses)
-        self.assertEqual(player.name, player_name)
-        self.assertEqual(player.max_health, max_health)
-        self.assertEqual(player.attack_speed, attack_speed)
-        self.assertEqual(player.weaknesses, weaknesses)
+        self.assertEqual(player.get_name(), player_name)
+        self.assertEqual(player.get_max_health(), max_health)
+        self.assertEqual(player.get_attack_speed(), attack_speed)
+        self.assertEqual(player.get_weaknesses(), weaknesses)
 
 if __name__ == '__main__':
     unittest.main()
