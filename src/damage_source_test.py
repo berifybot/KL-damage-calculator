9a585f40f0847fd644097c64d641447429b06e91
QuinnBerry <qberry97@outlook.com>
2024-02-08 19:49:19 -0700

    Add AttackType tests

--- src/damage_source_test.py
+++ src/damage_source_test.py
@@ -1,4 +1,4 @@
-from damage_source import Weapon, Element
+from damage_source import Weapon, Element, AttackType
 import unittest
 
 test_weapon = {
@@ -29,6 +29,34 @@ class TestDamageSource(unittest.TestCase):
         self.assertEqual(weapon.get_base_damage(), test_weapon['base_damage'])
         self.assertEqual(weapon.get_element(), getattr(Element, test_weapon['element']))
         self.assertEqual(weapon.get_attack_type().get_type(), test_weapon['damage_type'])
+
+class TestAttackType(unittest.TestCase):
+
+    def test_invalid_attack_type(self):
+        valid = AttackType.is_type_valid("invalid")
+        self.assertEqual(False, valid)
+
+    def test_valid_attack_types_lower_case(self):
+        melee_valid = AttackType.is_type_valid("melee")
+        range_valid = AttackType.is_type_valid("range")
+        magic_valid = AttackType.is_type_valid("magic")
+        ability_valid = AttackType.is_type_valid("ability")
+
+        self.assertEqual(True, melee_valid)
+        self.assertEqual(True, range_valid)
+        self.assertEqual(True, magic_valid)
+        self.assertEqual(True, ability_valid)
+
+    def test_valid_attack_types_random_case(self):
+        melee_valid = AttackType.is_type_valid("mElEe")
+        range_valid = AttackType.is_type_valid("RAnGe")
+        magic_valid = AttackType.is_type_valid("MAgiC")
+        ability_valid = AttackType.is_type_valid("ABIliTy")
+
+        self.assertEqual(True, melee_valid)
+        self.assertEqual(True, range_valid)
+        self.assertEqual(True, magic_valid)
+        self.assertEqual(True, ability_valid)
 
 if __name__ == '__main__':
     unittest.main()
