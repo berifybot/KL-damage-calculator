339f9a5aa65ef693c89145a31aef9e4030175df7
QuinnBerry <qberry97@outlook.com>
2024-02-10 21:28:43 -0700

    Add initial Element testing

---
+++ src/element_test.py
@@ -0,0 +1,23 @@
+import unittest
+from element import Element
+
+class TestElement(unittest.TestCase):
+
+    def test_invalid_creation(self):
+        invalid_element = Element.create("Invalid")
+        self.assertEqual(invalid_element, None)
+
+    def test_valid_creation_lower_case(self):
+        valid_element = Element.create("fire")
+        self.assertEqual(valid_element.get_type(), "fire")
+
+    def test_valid_creation_capital_case(self):
+        valid_element = Element.create("Fire")
+        self.assertEqual(valid_element.get_type(), "fire")
+
+    def test_valid_creation_mixed_case(self):
+        valid_element = Element.create("fIRe")
+        self.assertEqual(valid_element.get_type(), "fire")
+
+if __name__ == '__main__':
+    unittest.main()
