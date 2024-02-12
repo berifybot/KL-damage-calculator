33f9255ea7a1fd4e859a469ccccee31cbb0edc55
QuinnBerry <qberry97@outlook.com>
2024-02-11 19:26:58 -0700

    Account for low critical application chance elements (Wind, Lightning)

--- src/attack.py
+++ src/attack.py
@@ -1,5 +1,6 @@
 import math
 from entity import Entity
+from element import Element
 from battle_host import BattleHost
 
 class Attack():
@@ -50,6 +51,10 @@ class Attack():
         
     def __did_status_apply__(self, base_roll, status_roll) -> bool:
         if (base_roll == 5):
+            if (self.attacker.weapon.element == Element.WIND):
+                return False
+            if (self.attacker.weapon.element == Element.LIGHTNING):
+                return False
             if (self.attacker.has_increased_status_effect_chance()):
                 if (status_roll == 5):
                     return True
