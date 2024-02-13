58e2e198457969bc430af52f7b7a55d21688a56b
QuinnBerry <qberry97@outlook.com>
2024-02-12 20:35:33 -0700

    Add AttackTest skeleton, along with initial test object creators

---
+++ src/attack_test.py
@@ -0,0 +1,9 @@
+import unittest
+from attack import Attack
+from mock_objects import TestBattleHost
+
+class TestAttack(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/mock_objects.py
@@ -0,0 +1,42 @@
+from battle_host import BattleHost
+from damage_source import Weapon
+from entity import Enemy, Player
+
+class TestBattleHost(BattleHost):
+
+    def __init__(self, attack_speed, damage_per_roll, attack_type, roll):
+        self.attack_speed = attack_speed
+        self.get_damage_per_roll = damage_per_roll
+        self.attack_type = attack_type
+        self.roll = roll
+
+    def get_attack_speed(self) -> int:
+        return self.attack_speed
+    
+    def get_damage_per_roll(self) -> int:
+        return self.get_damage_per_roll
+    
+    def get_attack_type(self) -> int:
+        return self.attack_type
+    
+    def roll(self) -> int:
+        return self.roll()
+    
+    def report_roll_stats(self, roll_value, damage_dealt, enemy):
+        pass
+
+class TestEnemyCreator():
+
+    @classmethod
+    def create(cls, weapon: Weapon, name = "TestEnemy", max_health = 500, weaknesses = []) -> Enemy:
+        return Enemy(name, max_health, weapon, weaknesses)
+    
+class TestPlayerCreator():
+
+    @classmethod
+    def create(cls, weapon: Weapon, name = "TestPlayer", max_health = 500, attack_speed = 1, weaknesses= []) -> Player:
+        return Player(name, max_health, weapon, attack_speed, weaknesses)
+    
+    @classmethod
+    def create(cls, name = "TestWeapon", base_damage = 50, element = "Light", attack_type = "Melee"):
+        return Weapon(name, base_damage, element, attack_type)
