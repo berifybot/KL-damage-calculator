4e59bf9ff7f5601c6c56cd9ff2e98f8ebafc272a
QuinnBerry <qberry97@outlook.com>
2024-02-14 21:04:56 -0700

    Complete tests for __did_status_apply__()

--- src/attack.py
+++ src/attack.py
@@ -51,9 +51,9 @@ class Attack():
         
     def __did_status_apply__(self, base_roll, status_roll) -> bool:
         if (base_roll == 5):
-            if (self.attacker.weapon.element == Element.WIND):
+            if (self.attacker.weapon.element.get_type() == Element.WIND):
                 return False
-            if (self.attacker.weapon.element == Element.LIGHTNING):
+            if (self.attacker.weapon.element.get_type() == Element.LIGHTNING):
                 return False
             if (self.attacker.has_increased_status_effect_chance()):
                 if (status_roll == 5):
--- src/attack_test.py
+++ src/attack_test.py
@@ -72,7 +72,46 @@ class TestAttack(unittest.TestCase):
 
         applied = attack.__did_status_apply__(6, 5)
         self.assertEqual(applied, True)
+
+    def test_5_then_6_does_not_apply_status_for_low_chance_elements(self):
+        host = TestBattleHost(2, 25, "Melee", 1)
+        enemy = TestEnemyCreator().create(None)
+        weapon1 = TestWeaponCreator().create(element = "Wind")
+        player1 = TestPlayerCreator.create(weapon1)
+        attack1 = Attack(host, player1, enemy)
+
+        applied1 = attack1.__did_status_apply__(5, 6)
+        self.assertEqual(applied1, False)
+
+        weapon2 = TestWeaponCreator.create(element = "Lightning")
+        player2 = TestPlayerCreator().create(weapon2)
+        attack2 = Attack(host, player1, enemy)
+
+        applied2 = attack2.__did_status_apply__(5, 6)
+        self.assertEqual(applied2, False)
         
+    def test_5_then_5_applies_status_with_increased_chance(self):
+        host = TestBattleHost(2, 25, "Melee", 1)
+        enemy = TestEnemyCreator().create(None)
+        weapon = TestWeaponCreator().create()
+        player = TestPlayerCreator.create(weapon)
+        player.increased_status_effect_chance = True
+        attack = Attack(host, player, enemy)
+
+        applied = attack.__did_status_apply__(5, 5)
+        self.assertEqual(applied, True)
+
+    def test_6_then_4_applies_status_with_increased_chance(self):
+        host = TestBattleHost(2, 25, "Melee", 1)
+        enemy = TestEnemyCreator().create(None)
+        weapon = TestWeaponCreator().create()
+        player = TestPlayerCreator.create(weapon)
+        player.increased_status_effect_chance = True
+        attack = Attack(host, player, enemy)
+
+        applied = attack.__did_status_apply__(6, 4)
+        self.assertEqual(applied, True)
+           
 
 if __name__ == '__main__':
     unittest.main()
