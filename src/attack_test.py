72a6cf4ebd04db940134fa737ee8b11cbf811cfc
QuinnBerry <qberry97@outlook.com>
2024-02-17 15:38:02 -0700

    Add tests for __get_damage_from_roll__()

--- src/attack_test.py
+++ src/attack_test.py
@@ -111,7 +111,73 @@ class TestAttack(unittest.TestCase):
 
         applied = attack.__did_status_apply__(6, 4)
         self.assertEqual(applied, True)
-           
+
+    def test_damage_from_roll_1(self):
+        base_damage = 25
+        host = TestBattleHost(2, 25, "Melee", lambda a: 1)
+        enemy = TestEnemyCreator().create(None)
+        weapon = TestWeaponCreator().create(base_damage=base_damage)
+        player = TestPlayerCreator.create(weapon)
+        attack = Attack(host, player, enemy)
+
+        damage = attack.__get_damage_from_roll__(1)
+        self.assertEqual(0, damage)
+
+    def test_damage_from_roll_2(self):
+        base_damage = 25
+        host = TestBattleHost(2, 25, "Melee", 1)
+        enemy = TestEnemyCreator().create(None)
+        weapon = TestWeaponCreator().create(base_damage=base_damage)
+        player = TestPlayerCreator.create(weapon)
+        attack = Attack(host, player, enemy)
+
+        damage = attack.__get_damage_from_roll__(2)
+        self.assertEqual(0, damage)
+
+    def test_damage_from_roll_3(self):
+        base_damage = 25
+        host = TestBattleHost(2, 25, "Melee", 1)
+        enemy = TestEnemyCreator().create(None)
+        weapon = TestWeaponCreator().create(base_damage=base_damage)
+        player = TestPlayerCreator.create(weapon)
+        attack = Attack(host, player, enemy)
+
+        damage = attack.__get_damage_from_roll__(3)
+        self.assertEqual(12, damage)
+
+    def test_damage_from_roll_4(self):
+        base_damage = 25
+        host = TestBattleHost(2, 25, "Melee", 1)
+        enemy = TestEnemyCreator().create(None)
+        weapon = TestWeaponCreator().create(base_damage=base_damage)
+        player = TestPlayerCreator.create(weapon)
+        attack = Attack(host, player, enemy)
+
+        damage = attack.__get_damage_from_roll__(4)
+        self.assertEqual(25, damage)
+
+    def test_damage_from_roll_5(self):
+        base_damage = 25
+        host = TestBattleHost(2, 25, "Melee", lambda: 1 )
+        enemy = TestEnemyCreator().create(None)
+        weapon = TestWeaponCreator().create(base_damage=base_damage)
+        player = TestPlayerCreator.create(weapon)
+        attack = Attack(host, player, enemy)
+
+        damage = attack.__get_damage_from_roll__(5)
+        self.assertEqual(28, damage)
+
+    def test_damage_from_roll_6(self):
+        base_damage = 25
+        host = TestBattleHost(2, 25, "Melee", lambda: 1 )
+        enemy = TestEnemyCreator().create(None)
+        weapon = TestWeaponCreator().create(base_damage=base_damage)
+        player = TestPlayerCreator.create(weapon)
+        attack = Attack(host, player, enemy)
+
+        damage = attack.__get_damage_from_roll__(6)
+        self.assertEqual(30, damage)
+
 
 if __name__ == '__main__':
     unittest.main()
